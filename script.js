c3a7005fe7c98d782bd322ef42257e646b68bfe2
sarath2905 <bsarath2905@gmail.com>
2024-06-26 19:05:49 +0530

    change

--- script.js
+++ script.js
@@ -27,3 +27,43 @@ console.log(typeof age)
 
 //concatenate
 console.log('My name is ' +pname+ ' and age is ' +age);
+
+//array
+arrayOne=["John",30,true,null,undefined];
+console.log(arrayOne);
+arrayOne.push("apple");
+console.log(arrayOne);
+arrayOne.pop();
+console.log(arrayOne);
+arrayOne.unshift("Mango");
+console.log(arrayOne);
+arrayOne[3]="Orange";
+console.log(arrayOne);
+
+console.log(arrayOne.length);
+
+//to delete starting from a position
+arrayOne.splice(3,2);
+console.log(arrayOne);
+
+let valueOnee="2";
+let valueTwoo=2;
+
+console.log(valueOnee==valueTwoo);
+console.log(valueOnee===valueTwoo);
+
+
+//conditional
+let value=16
+
+if (value>=18)
+    {
+        console.log("adult");
+
+    }else{
+        console.log("minor");
+    }
+
+    let agee=21;
+    let type=(age>=18)?"adult":"Minor";
+    console.log(type);    
