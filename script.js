97e262ddf003e3172c61e2c38e7f1d56d869a515
sarath2905 <bsarath2905@gmail.com>
2024-06-26 18:30:20 +0530

    Data Type config

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+
+var a=10;
+console.log(a);
+
+var a="name"
+console.log(a);
+let name="messi"
+console.log(name)
+
+const pi=3.14
+console.log(pi);
+
+//type conversion
+let valueOne=2;
+let valueTwo="2";
+console.log(valueOne+valueTwo);
+console.log(valueOne+parseInt(valueTwo));
+
+
+const pname="john";
+const age=30;
+const isCool="true";
+
+console.log(typeof pname)
+console.log(typeof age)
+
+//concatenate
+console.log('My name is ' +pname+ ' and age is ' +age);
